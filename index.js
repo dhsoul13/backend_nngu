ed8eee4fa0cf57970492038aae95dc740babb684
dhsoul13 <muxreev.dan2@yandex.ru>
2022-04-23 15:27:23 +0300

    "0.0.2"

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Node.js")
+const mode = process.env.MODE;
+if(mode === "dev"){
+    console.log(mode)
+}
+let  userName = process.argv[2];
+let  userAge = process.argv[3];
+if (userName){
+    console.log(`Hello, ${userName}. Your age is ${userAge || "not"}`)
+    process.exit(1);
+} else {
+    console.log(`Hello, Node js`)
+}
+process.exit(0);
