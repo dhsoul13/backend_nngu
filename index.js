5b751e9c4d0bcc543c5b9975ac1a3b1b01b5286d
dhsoul13 <muxreev.dan2@yandex.ru>
2022-04-26 09:49:45 +0300

    "homework_1"

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const mode = process.env.MODE;
-if(mode === "dev"){
-    console.log(mode)
-}
-let  userName = process.argv[2];
-let  userAge = process.argv[3];
-if (userName){
-    console.log(`Hello, ${userName}. Your age is ${userAge || "not"}`)
-    process.exit(1);
-} else {
-    console.log(`Hello, Node js`)
-}
-process.exit(0);
+let a = 5
