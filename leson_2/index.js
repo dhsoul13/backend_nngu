b370242b53d7239e510aacffaccb9913cd8c7347
dhsoul13 <muxreev.dan2@yandex.ru>
2022-05-04 17:54:32 +0300

    "homework_2"

---
+++ leson_2/index.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output:process.stdout,
+});
+let a, b, c;
+const callBackOnString = ()=>{
+    
+};
+
+rl.question(("Введите a: =>"),(ans)=>{
+    
+})
